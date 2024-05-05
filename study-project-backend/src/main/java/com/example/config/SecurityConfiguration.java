95ed752b51abd35693b336c70fb718902a054440
CoderLKai <2235043566@qq.com>
2024-05-05 16:57:15 +0800

    配置登录验证接口返回JSON数据

---
+++ study-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.config;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.entity.RestBean;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(conf -> {
+                    conf.loginProcessingUrl("/api/auth/login");
+                    conf.successHandler(this::onAuthenticationSuccess);
+                    conf.failureHandler(this::onAuthenticationFailure);
+                })
+                .logout(conf -> {
+                    conf.logoutUrl("/api/auth/logout");
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(conf ->{
+                    conf.authenticationEntryPoint(this::onAuthenticationFailure);
+                })
+                .build();
+    }
+
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.success("登录成功")));
+    }
+
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401, exception.getMessage())));
+    }
+}
